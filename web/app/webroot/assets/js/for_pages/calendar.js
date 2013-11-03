b494099ce2e30ab7909fa038ac82173c6b755442
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2013-11-03 14:22:29 +0000

    new calendar values
    Signed-off-by: Ondrej Rafaj <ondrej.rafaj@fuerteint.com>

--- web/app/webroot/assets/js/for_pages/calendar.js
+++ web/app/webroot/assets/js/for_pages/calendar.js
@@ -29,38 +29,38 @@
       editable: true,
       events: [
         {
-          title: "All Day Event",
+          title: "iDeviant",
           start: new Date(y, m, 1)
         }, {
-          title: "Long Event",
+          title: "Ford",
           start: new Date(y, m, d - 5),
           end: new Date(y, m, d - 2)
         }, {
           id: 999,
-          title: "Repeating Event",
+          title: "Pockupation",
           start: new Date(y, m, d - 3, 16, 0),
           allDay: false
         }, {
           id: 999,
-          title: "Repeating Event",
+          title: "iJenkins",
           start: new Date(y, m, d + 4, 16, 0),
           allDay: false
         }, {
-          title: "Meeting",
+          title: "Calpol",
           start: new Date(y, m, d, 10, 30),
           allDay: false
         }, {
-          title: "Lunch",
+          title: "StickerTag",
           start: new Date(y, m, d, 12, 0),
           end: new Date(y, m, d, 14, 0),
           allDay: false
         }, {
-          title: "Birthday Party",
+          title: "iSpeedtest",
           start: new Date(y, m, d + 1, 19, 0),
           end: new Date(y, m, d + 1, 22, 30),
           allDay: false
         }, {
-          title: "Click for Google",
+          title: "Barclays demo",
           start: new Date(y, m, 28),
           end: new Date(y, m, 29),
           url: "http://google.com/"
