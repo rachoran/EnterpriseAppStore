51c5d8074df4e4309ca7278e9af5ac903797865b
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2013-11-05 00:12:54 +0000

    wip
    Signed-off-by: Ondrej Rafaj <ondrej.rafaj@fuerteint.com>

--- web/app/webroot/assets/js/for_pages/applications-edit.js
+++ web/app/webroot/assets/js/for_pages/applications-edit.js
@@ -2,7 +2,7 @@ $('#binaryUpload').fileupload({
     url: env.baseUrl + 'thirdparty/jQuery-File-Upload/server/php/index.php',
     dataType: 'json',
     done: function (e, data) {
-    	//alert(JSON.parse(data));
+    	alert(data.files.length);
     	
     	$.each(data.files, function (index, file) {
             $('#wqefcqwrfqrfq').text($('#wqefcqwrfqrfq').text() + "\n" + file.name);
@@ -11,5 +11,8 @@ $('#binaryUpload').fileupload({
     progressall: function (e, data) {
         var progress = parseInt(data.loaded / data.total * 100, 10);
         $('#binaryUploadProgress .progress-bar').css('width', progress + '%');
+        alert(env.baseUrl + 'thirdparty/jQuery-File-Upload/server/php/index.php');
     }
+}).error(function (jqXHR, textStatus, errorThrown) {
+	alert(errorThrown);
 }).prop('disabled', !$.support.fileInput).parent().addClass($.support.fileInput ? undefined : 'disabled');
