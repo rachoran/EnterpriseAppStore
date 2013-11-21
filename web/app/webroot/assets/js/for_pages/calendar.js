76a2922fa82f2689c8e80edbc1cf6a9be4149dcf
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2013-11-21 19:32:30 +0000

    fixing calendar
    Signed-off-by: Ondrej Rafaj <ondrej.rafaj@fuerteint.com>

--- web/app/webroot/assets/js/for_pages/calendar.js
+++ web/app/webroot/assets/js/for_pages/calendar.js
@@ -13,6 +13,27 @@
 				m = date.getMonth();
 				y = date.getFullYear();
 				
+				var eventsData = jsonData.data;
+				for (var i = 0; i < eventsData.length; i++) {
+					eventsData[i].start = new Date(eventsData[i].start * 1000);
+					eventsData[i].end = new Date(eventsData[i].end * 1000);
+				}
+/*
+				var eventsData = [
+					{
+						title: "iSpeedtest",
+						start: new Date(y, m, d + 1, 19, 0),
+						end: new Date(y, m, d + 1, 22, 30),
+						allDay: false
+					}, {
+						title: "Barclays demo",
+						start: new Date(y, m, 28),
+						end: new Date(y, m, 29),
+						url: env.baseUrl + 'users/edit/10/yo'
+					}
+				];
+*/
+				
 				return calendar = $("#calendar").fullCalendar({
 					header: {
 						left: "prev,next today",
